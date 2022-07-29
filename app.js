f1c3cddb055071f6d44a3fd8b76fff22193cb7a3
Isadora Áraújo <isadoraraujo13@gmail.com>
2022-07-29 16:20:40 -0300

    readline funciona e array é retornado em ordem alfabética

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
+const readline = require("readline")
+const rl = readline.createInterface ({
+    input: process.stdin,
+    output: process.stdout
+})
+
 let ordenadorAZ = new Promise((res, rej) => {
     res()
     rej()
 })
 
 ordenadorAZ.then(
-    (propriedadesCSS) => {
-        console.log(propriedadesCSS = ['font-size', 'text-align', 'backgroung-color'].sort())
+    () => {
+        rl.question('Digite uma propriedade CSS e eu irei listar de A-Z: ', (propriedade) => {
+            let propriedadesOrdenadas = propriedade.split(' ').sort( )
+            console.log(propriedadesOrdenadas)
+        })
     },
     (error) => {
         console.log(error)
